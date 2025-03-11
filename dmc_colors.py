fc8722055aef7e7dbab83e9f09a8848583964779
FiBoot <fiboot89@gmail.com>
2025-03-11 22:31:14 +0100

    wip dmc color palettecolor process

---
+++ dmc_colors.py
@@ -0,0 +1,492 @@
+# https://dmc.crazyartzone.com/dmc_to_hex.php?color=&ob=dmc
+DMC_RGB_COLORS = [
+  255,255,255,	#B5200	Snow White
+  252,251,248,	#White	White
+  240,234,218,	#Ecru	Ecru
+  # 227,227,230,	#01	White Tin
+  # 215,215,216,	#02	Tin
+  # 184,184,187,	#03	Tin Medium
+  # 174,174,177,	#04	Tin Dark
+  # 227,204,190,	#05	Driftwood Light
+  # 220,198,184,	#06	Driftwood Medium Light
+  # 143,123,110,	#07	Driftwood
+  # 106,80,70,    #08	Driftwood Dark
+  # 85,32,14,     #09	Cocoa Very Dark
+  # 237,254,217,	#10	Tender Green Very Light
+  # 226,237,181,	#11	Tender Green Light
+  # 205,217,154,	#12	Tender Green
+  # 191,246,224,	#13	Nile Green Medium Light
+  # 208,251,178,	#14	Apple Green Pale
+  # 209,237,164,	#15	Apple Green
+  # 201,194,88,   #16	Chartreuse Light
+  # 229,226,114,	#17	Yellow Plum Light
+  # 217,213,109,	#18	Yellow Plum
+  # 247,201,95,   #19	Autumn Gold medium Light
+  # 247,175,147,	#20	Shrimp
+  # 215,153,130,	#21	Alizarin Light
+  # 188,96,78,    #22	Alizarin
+  # 237,226,237,	#23	Apple Blossom
+  # 224,215,238,	#24	White Lavender
+  # 218,210,233,	#25	Lavender Ultra Light
+  # 215,202,230,	#26	Lavender Pale
+  # 240,238,249,	#27	White Violet
+  # 144,134,169,	#28	Eggplant Medium Light
+  # 103,64,118,   #29	Eggplant
+  # 125,119,165,	#30	Blueberry Medium Light
+  # 80,81,141,    #31	Blueberry
+  # 77,46,138,    #32	Blueberry Dark
+  # 156,89,158,   #33	Fuschia
+  # 125,48,100,   #34	Fuschia Dark
+  # 70,5,45,	    #35	Fuschia Very Dark
+  171,2,73,	    #150	Dusty Rose Ultra Very Dark
+  240,206,212,	#151	Dusty Rose Very Light
+  226,160,153,	#152	Shell Pink Medium Light
+  230,204,217,	#153	Violet Very Light
+  87,36,51,	    #154	Grape Very Dark
+  152,145,182,	#155	Blue Violet Medium Dark
+  163,174,209,	#156	Blue Violet Medium Light
+  187,195,217,	#157	Cornflower Blue Very Light
+  76,82,110,    #158	Cornflower Blue Very Dark
+  199,202,215,	#159	Blue Gray Light
+  153,159,183,	#160	Blue Gray Medium
+  120,128,164,	#161	Blue Gray
+  219,236,245,	#162	Blue Ultra Very Light
+  77,131,97,    #163	Celadon Green Medium
+  200,216,184,	#164	Forest Green Light
+  239,244,164,	#165	Moss Green Very Light
+  192,200,64,   #166	Moss Green Medium Light
+  167,124,73,   #167	Yellow Beige Very Dark
+  209,209,209,	#168	Pewter Very Light
+  132,132,132,	#169	Pewter Light
+  131,91,139,   #208	Lavender Very Dark
+  163,123,167,	#209	Lavender Dark
+  195,159,195,	#210	Lavender Medium
+  227,203,227,	#211	Lavender Light
+  136,62,67,    #221	Shell Pink Very Dark
+  204,132,124,	#223	Shell Pink Light
+  235,183,175,	#224	Shell Pink Very Light
+  255,223,213,	#225	Shell Pink Ultra Very Light
+  111,47,0,	    #300	Mahogany Very Dark
+  179,95,43,    #301	Mahogany Medium
+  183,31,51,    #304	Red Medium
+  253,237,84,   #307	Lemon
+  86,74,74,	    #309	Rose Dark
+  0,0,0,        #310	Black
+  28,80,102,    #311	Wedgewood Ultra Very Dark
+  53,102,139,   #312	Baby Blue Very Dark
+  129,73,82,    #315	Antique Mauve Medium Dark
+  183,115,127,	#316	Antique Mauve Medium
+  108,108,108,	#317	Pewter Gray
+  171,171,171,	#318	Steel Gray Light
+  32,95,46,	    #319	Pistachio Grn Very Dark
+  105,136,90,   #320	Pistachio Green Medium
+  199,43,59,    #321	Red
+  90,143,184,   #322	Baby Blue Dark
+  179,59,75,    #326	Rose Very Dark
+  99,54,102,    #327	Violet Dark
+  92,84,120,    #333	Blue Violet Very Dark
+  115,159,193,	#334	Baby Blue Medium
+  238,84,110,   #335	Rose
+  37,59,115,	  #336	Navy Blue
+  173,167,199,	#340	Blue Violet Medium
+  183,191,221,	#341	Blue Violet Light
+  191,45,45,	  #347	Salmon Very Dark
+  210,16,53,	  #349	Coral Dark
+  224,72,72,	  #350	Coral Medium
+  233,106,103,	#351	Coral
+  253,156,151,	#352	Coral Light
+  254,215,204,	#353	Peach
+  152,68,54,	  #355	Terra Cotta Dark
+  197,106,91,	  #356	Terra Cotta Medium
+  97,122,82,	  #367	Pistachio Green Dark
+  166,194,152,	#368	Pistachio Green Light
+  215,237,204,	#369	Pistachio Green Very Light
+  184,157,100,	#370	Mustard Medium
+  191,166,113,	#371	Mustard
+  204,183,132,	#372	Mustard Light
+  143,67,15,	  #400	Mahogany Dark
+  247,167,119,	#402	Mahogany Very Light
+  187,129,97,	  #407	Desert Sand Medium
+  86,86,86,	    #413	Pewter Gray Dark
+  140,140,140,	#414	Steel Gray Dark
+  211,211,214,	#415	Pearl Gray
+  160,112,66,	  #420	Hazelnut Brown Dark
+  198,159,123,	#422	Hazelnut Brown Light
+  122,69,31,	  #433	Brown Medium
+  152,94,51,	  #434	Brown Light
+  184,119,72,	  #435	Brown Very Light
+  203,144,81,	  #436	Tan
+  228,187,142,	#437	Tan Light
+  255,214,0,	  #444	Lemon Dark
+  255,251,139,	#445	Lemon Light
+  145,123,115,	#451	Shell Gray Dark
+  192,179,174,	#452	Shell Gray Medium
+  215,206,203,	#453	Shell Gray Light
+  114,132,60,	  #469	Avocado Green
+  148,171,79,	  #470	Avocado Grn Light
+  174,191,121,	#471	Avocado Grn Very Light
+  216,228,152,	#472	Avocado Green Ultra Light
+  167,19,43,	  #498	Red Dark
+  4,77,51,	    #500	Blue Green Very Dark
+  57,111,82,	  #501	Blue Green Dark
+  91,144,113,	  #502	Blue Green
+  123,172,148,	#503	Blue Green Medium
+  196,222,204,	#504	Blue Green Very Light
+  51,131,98,	  #505	Jade Green
+  59,118,143,	  #517	Wedgewood Dark
+  79,147,167,	  #518	Wedgewood Light
+  126,177,200,	#519	Sky Blue
+  102,109,79,	  #520	Fern Green Dark
+  150,158,126,	#522	Fern Green
+  171,177,151,	#523	Fern Green Light
+  196,205,172,	#524	Fern Green Very Light
+  99,100,88,	  #535	Ash Gray Very Light
+  242,227,206,	#543	Beige Brown Ultra Very Light
+  92,24,78,	    #550	Violet Very Dark
+  128,58,107,	  #552	Violet Medium
+  163,99,139,	  #553	Violet
+  219,179,203,	#554	Violet Light
+  44,106,69,	  #561	Celadon Green VD
+  83,151,106,	  #562	Jade Medium
+  143,192,152,	#563	Jade Light
+  167,205,175,	#564	Jade Very Light
+  136,141,51,	  #580	Moss Green Dark
+  167,174,56,	  #581	Moss Green
+  91,163,179,	  #597	Turquoise
+  144,195,204,	#598	Turquoise Light
+  205,47,99,	  #600	Cranberry Very Dark
+  209,40,106,	  #601	Cranberry Dark
+  226,72,116,	  #602	Cranberry Medium
+  255,164,190,	#603	Cranberry
+  255,176,190,	#604	Cranberry Light
+  255,192,205,	#605	Cranberry Very Light
+  250,50,3,	    #606	Orange Red Bright
+  253,93,53,	  #608	Burnt Orange Bright
+  121,96,71,	  #610	Drab Brown Dark
+  150,118,86,	  #611	Drab Brown
+  188,154,120,	#612	Drab Brown Light
+  220,196,170,	#613	Drab Brown Very Light
+  135,85,57,	  #632	Desert Sand Ultra Very Dark
+  133,123,97,	  #640	Beige Gray Very Dark
+  164,152,120,	#642	Beige Gray Dark
+  221,216,203,	#644	Beige Gray Medium
+  110,101,92,	  #645	Beaver Gray Very Dark
+  135,125,115,	#646	Beaver Gray Dark
+  176,166,156,	#647	Beaver Gray Medium
+  188,180,172,	#648	Beaver Gray Light
+  227,29,66,	  #666	Bright Red
+  229,206,151,	#676	Old Gold Light
+  245,236,203,	#677	Old Gold Very Light
+  188,141,14,	  #680	Old Gold Dark
+  5,101,23, 	  #699	Green
+  7,115,27, 	  #700	Green Bright
+  63,143,41,	  #701	Green Light
+  71,167,47,	  #702	Kelly Green
+  123,181,71,	  #703	Chartreuse
+  158,207,52,	  #704	Chartreuse Bright
+  255,251,239,	#712	Cream
+  156,36,98,	  #718	Plum
+  229,92,31,	  #720	Orange Spice Dark
+  242,120,66,	  #721	Orange Spice Medium
+  247,151,111,	#722	Orange Spice Light
+  255,200,64, 	#725	Topaz Medium Light
+  253,215,85,	  #726	Topaz Light
+  255,241,175,	#727	Topaz Very Light
+  228,180,104,	#728	Topaz
+  208,165,62,	  #729	Old Gold Medium
+  130,123,48,	  #730	Olive Green Very Dark
+  147,139,55,	  #731	Olive Green Dark
+  148,140,54,	  #732	Olive Green
+  188,179,76,	  #733	Olive Green Medium
+  199,192,119,	#734	Olive Green Light
+  236,204,158,	#738	Tan Very Light
+  248,228,200,	#739	Tan Ultra Very Light
+  255,139,0,	  #740	Tangerine
+  255,163,43,	  #741	Tangerine Medium
+  255,191,87,	  #742	Tangerine Light
+  254,211,118,	#743	Yellow Medium
+  255,231,147,	#744	Yellow Pale
+  255,233,173,	#745	Yellow Pale Light
+  252,252,238,	#746	Off White
+  229,252,253,	#747	Peacock Blue Very Light
+  247,203,191,	#754	Peach Light
+  238,170,155,	#758	Terra Cotta Very Light
+  245,173,173,	#760	Salmon
+  255,201,201,	#761	Salmon Light
+  236,236,236,	#762	Pearl Gray Very Light
+  228,236,212,	#772	Yellow Green Very Light
+  217,235,241,	#775	Baby Blue Very Light
+  252,176,185,	#776	Pink Medium
+  145,53,70,	  #777	Raspberry Very Dark
+  223,179,187,	#778	Antique Mauve Very Light
+  98,75,69,	    #779	Cocoa Dark
+  148,99,26,	  #780	Topaz Ultra Very Dark
+  162,109,32,	  #781	Topaz Very Dark
+  174,119,32,	  #782	Topaz Dark
+  206,145,36,	  #783	Topaz Medium
+  70,69,99,	    #791	Cornflower Blue Very Dark
+  85,91,123,	  #792	Cornflower Blue Dark
+  112,125,162,	#793	Cornflower Blue Medium
+  143,156,193,	#794	Cornflower Blue Light
+  17,65,109,	  #796	Royal Blue Dark
+  19,71,125,	  #797	Royal Blue
+  70,106,142, 	#798	Delft Blue Dark
+  116,142,182,	#799	Delft Blue Medium
+  192,204,222,	#800	Delft Blue Pale
+  101,57,25,	  #801	Coffee Brown Dark
+  44,89,124,	  #803	Baby Blue Ultra Very Dark
+  61,149,165,	  #806	Peacock Blue Dark
+  100,171,186,	#807	Peacock Blue
+  148,168,198,	#809	Delft Blue
+  161,194,215,	#813	Blue Light
+  123,0,27,	    #814	Garnet Dark
+  135,7,31,	    #815	Garnet Medium
+  151,11,35,	  #816	Garnet
+  187,5,31,	    #817	Coral Red Very Dark
+  255,223,217,	#818	Baby Pink
+  255,238,235,	#819	Baby Pink Light
+  14,54,92,	    #820	Royal Blue Very Dark
+  231,226,211,	#822	Beige Gray Light
+  33,48,99,	    #823	Navy Blue Dark
+  57,105,135,	  #824	Blue Very Dark
+  71,129,165,	  #825	Blue Dark
+  107,158,191,	#826	Blue Medium
+  189,221,237,	#827	Blue Very Light
+  197,232,237,	#828	Sky Blue Very Light
+  126,107,66,	  #829	Golden Olive Very Dark
+  141,120,75,	  #830	Golden Olive Dark
+  170,143,86,	  #831	Golden Olive Medium
+  189,155,81,	  #832	Golden Olive
+  200,171,108,	#833	Golden Olive Light
+  219,190,127,	#834	Golden Olive Very Light
+  89,73,55,     #838	Beige Brown Very Dark
+  103,85,65,	  #839	Beige Brown Dark
+  154,124,92,	  #840	Beige Brown Medium
+  182,155,126,	#841	Beige Brown Light
+  209,186,161,	#842	Beige Brown Very Light
+  72,72,72,	    #844	Beaver Gray Ultra Dark
+  131,94,57,	  #869	Hazelnut Brown Very Dark
+  23,73,35,	    #890	Pistachio Grn Ultra Very Dark
+  255,87,115,	  #891	Carnation Dark
+  255,121,140,	#892	Carnation Medium
+  252,144,162,	#893	Carnation Light
+  255,178,187,	#894	Carnation Very Light
+  27,83,0,	    #895	Hunter Green Very Dark
+  73,42,19,	    #898	Coffee Brown Very Dark
+  242,118,136,	#899	Rose Medium
+  209,88,7,	    #900	Burnt Orange Dark
+  130,38,55,	  #902	Garnet Very Dark
+  85,120,34,	  #904	Parrot Green Very Dark
+  98,138,40,	  #905	Parrot Green Dark
+  127,179,53,	  #906	Parrot Green Medium
+  199,230,102,	#907	Parrot Green Light
+  21,111,73,	  #909	Emerald Green Very Dark
+  24,126,86,	  #910	Emerald Green Dark
+  24,144,101,	  #911	Emerald Green Medium
+  27,157,107,	  #912	Emerald Green Light
+  109,171,119,	#913	Nile Green Medium
+  130,0,67,	    #915	Plum Dark
+  155,19,89,	  #917	Plum Medium
+  130,52,10,	  #918	Red Copper Dark
+  166,69,16,	  #919	Red Copper
+  172,84,20,	  #920	Copper Medium
+  198,98,24,	  #921	Copper
+  226,115,35,	  #922	Copper Light
+  86,106,106,	  #924	Gray Green Vy Dark
+  152,174,174,	#926	Gray Green Medium
+  189,203,203,	#927	Gray Green Light
+  221,227,227,	#928	Gray Green Very Light
+  69,92,113,	  #930	Antique Blue Dark
+  106,133,158,	#931	Antique Blue Medium
+  162,181,198,	#932	Antique Blue Light
+  49,57,25,	    #934	Avocado Grn Black
+  66,77,33,	    #935	Avocado Green Dark
+  76,88,38,	    #936	Avocado Green Very Dark
+  98,113,51,	  #937	Avocado Green Medium
+  54,31,14,	    #938	Coffee Brown Ultra Dark
+  27,40,83,	    #939	Navy Blue Very Dark
+  61,147,132,	  #943	Green Bright Medium
+  251,213,187,	#945	Tawny
+  235,99,7,	    #946	Burnt Orange Medium
+  255,123,77,	  #947	Burnt Orange
+  254,231,218,	#948	Peach Very Light
+  238,211,196,	#950	Desert Sand Light
+  255,226,207,	#951	Tawny Light
+  136,186,145,	#954	Nile Green
+  162,214,173,	#955	Nile Green Light
+  255,145,145,	#956	Geranium
+  253,181,181,	#957	Geranium Pale
+  62,182,161,	  #958	Sea Green Dark
+  89,199,180,	  #959	Sea Green Medium
+  207,115,115,	#961	Dusty Rose Dark
+  230,138,138,	#962	Dusty Rose Medium
+  255,215,215,	#963	Dusty Rose Ultra Very Light
+  169,226,216,	#964	Sea Green Light
+  185,215,192,	#966	Jade Ultra Very Light
+  255,222,213,	#967	Apricot Very Light
+  247,139,19,	  #970	Pumpkin Light
+  246,127,0,	  #971	Pumpkin
+  255,181,21,	  #972	Canary Deep
+  255,227,0,	  #973	Canary Bright
+  145,79,18,	  #975	Golden Brown Dark
+  194,129,66,	  #976	Golden Brown Medium
+  220,156,86,	  #977	Golden Brown Light
+  64,82,48,	    #986	Forest Green Very Dark
+  88,113,65,	  #987	Forest Green Dark
+  115,139,91,	  #988	Forest Green Medium
+  141,166,117,	#989	Forest Green
+  71,123,110,	  #991	Aquamarine Dark
+  111,174,159,	#992	Aquamarine Light
+  144,192,180,	#993	Aquamarine Very Light
+  38,150,182,	  #995	Electric Blue Dark
+  48,194,236,	  #996	Electric Blue Medium
+  137,138,88,	  #3011	Khaki Green Dark
+  166,167,93,	  #3012	Khaki Green Medium
+  185,185,130,	#3013	Khaki Green Light
+  79,75,65,	    #3021	Brown Gray Very Dark
+  142,144,120,	#3022	Brown Gray Medium
+  177,170,151,	#3023	Brown Gray Light
+  235,234,231,	#3024	Brown Gray Very Light
+  75,60,42,	    #3031	Mocha Brown Very Dark
+  179,159,139,	#3032	Mocha Brown Medium
+  227,216,204,	#3033	Mocha Brown Very Light
+  149,111,124,	#3041	Antique Violet Medium
+  183,157,167,	#3042	Antique Violet Light
+  188,150,106,	#3045	Yellow Beige Dark
+  216,188,154,	#3046	Yellow Beige Medium
+  231,214,193,	#3047	Yellow Beige Light
+  95,102,72,	  #3051	Green Gray Dark
+  136,146,104,	#3052	Green Gray Medium
+  156,164,130,	#3053	Green Gray
+  196,142,112,	#3064	Desert Sand
+  230,232,232,	#3072	Beaver Gray Very Light
+  253,249,205,	#3078	Golden Yellow Very Light
+  184,210,230,	#3325	Baby Blue Light
+  251,173,180,	#3326	Rose Light
+  227,109,109,	#3328	Salmon Dark
+  255,131,111,	#3340	Apricot Medium
+  252,171,152,	#3341	Apricot
+  27,89,21, 	  #3345	Hunter Green Dark
+  64,106,58,	  #3346	Hunter Green
+  113,147,92,	  #3347	Yellow Green Medium
+  204,217,177,	#3348	Yellow Green Light
+  188,67,101,	  #3350	Dusty Rose Ultra Dark
+  228,166,172,	#3354	Dusty Rose Light
+  94,107,71,	  #3362	Pine Green Dark
+  114,130,86,	  #3363	Pine Green Medium
+  131,151,95,	  #3364	Pine Green
+  30,17,8,	    #3371	Black Brown
+  197,73,137,	  #3607	Plum Light
+  234,156,196,	#3608	Plum Very Light
+  244,174,213,	#3609	Plum Ultra Light
+  136,21,49,	  #3685	Mauve Very Dark
+  201,107,112,	#3687	Mauve
+  231,169,172,	#3688	Mauve Medium
+  251,191,194,	#3689	Mauve Light
+  255,121,146,	#3705	Melon Dark
+  255,173,188,	#3706	Melon Medium
+  255,203,213,	#3708	Melon Light
+  241,135,135,	#3712	Salmon Medium
+  255,226,226,	#3713	Salmon Very Light
+  255,189,189,	#3716	Dusty Rose Medium Very Light
+  161,75,81,	  #3721	Shell Pink Dark
+  188,108,100,	#3722	Shell Pink Medium
+  155,91,102,	  #3726	Antique Mauve Dark
+  219,169,178,	#3727	Antique Mauve Light
+  218,103,131,	#3731	Dusty Rose Very Dark
+  232,135,155,	#3733	Dusty Rose
+  120,87,98,	  #3740	Antique Violet Dark
+  215,203,211,	#3743	Antique Violet Very Light
+  119,107,152,	#3746	Blue Violet Dark
+  211,215,237,	#3747	Blue Violet Very Light
+  56,76,94,	    #3750	Antique Blue Very Dark
+  199,209,219,	#3752	Antique Blue Very Light
+  219,226,233,	#3753	Antique Blue Ultra Very Light
+  147,180,206,	#3755	Baby Blue
+  238,252,252,	#3756	Baby Blue Ultra Very Light
+  62,133,162,	  #3760	Wedgewood Medium
+  172,216,226,	#3761	Sky Blue Light
+  52,127,140,	  #3765	Peacock Blue Very Dark
+  153,207,217,	#3766	Peacock Blue Light
+  101,127,127,	#3768	Gray Green Dark
+  255,238,227,	#3770	Tawny Vy Light
+  244,187,169,	#3771	Terra Cotta Ultra Very Light
+  160,108,80,	  #3772	Desert Sand Very Dark
+  182,117,82,	  #3773	Desert Sand Dark
+  243,225,215,	#3774	Desert Sand Very Light
+  207,121,57,	  #3776	Mahogany Light
+  134,48,34,	  #3777	Terra Cotta Very Dark
+  217,137,120,	#3778	Terra Cotta Light
+  248,202,200,	#3779	Rosewood Ultra Very Light
+  107,87,67,	  #3781	Mocha Brown Dark
+  210,188,166,	#3782	Mocha Brown Light
+  98,93,80,	    #3787	Brown Gray Dark
+  127,106,85, 	#3790	Beige Gray Ultra Dark
+  66,66,66,	    #3799	Pewter Gray Very Dark
+  231,73,103,	  #3801	Melon Very Dark
+  113,65,73,	  #3802	Antique Mauve Very Darkv
+  171,51,87,	  #3803	Mauve Dark
+  224,40,118,	  #3804	Cyclamen Pink Dark
+  243,71,139,	  #3805	Cyclamen Pink
+  255,140,174,	#3806	Cyclamen Pink Light
+  96,103,140,	  #3807	Cornflower Blue
+  54,105,112,	  #3808	Turquoise Ultra Very Dark
+  63,124,133,	  #3809	Turquoise Vy Dark
+  72,142,154,	  #3810	Turquoise Dark
+  188,227,230,	#3811	Turquoise Very Light
+  47,140,132,	  #3812	Sea Green Very Dark
+  178,212,189,	#3813	Blue Green Light
+  80,139,125,	  #3814	Aquamarine
+  71,119,89,	  #3815	Celadon Green Dark
+  101,165,125,	#3816	Celadon Green
+  153,195,170,	#3817	Celadon Green Light
+  17,90,59,	    #3818	Emerald Green Ultra Very Dark
+  224,232,104,	#3819	Moss Green Light
+  223,182,95,	  #3820	Straw Dark
+  243,206,117,	#3821	Straw
+  246,220,152,	#3822	Straw Light
+  255,253,227,	#3823	Yellow Ultra Pale
+  254,205,194,	#3824	Apricot Light
+  253,189,150,	#3825	Pumpkin Pale
+  173,114,57,	  #3826	Golden Brown
+  247,187,119,	#3827	Golden Brown Pale
+  183,139,97,	  #3828	Hazelnut Brown
+  169,130,4,	  #3829	Old Gold Vy Dark
+  185,85,68,	  #3830	Terra Cotta
+  179,47,72,	  #3831	Raspberry Dark
+  219,85,110,	  #3832	Raspberry Medium
+  234,134,153,	#3833	Raspberry Light
+  114,55,93,	  #3834	Grape Dark
+  148,96,131, 	#3835	Grape Medium
+  186,145,170,	#3836	Grape Light
+  108,58,110, 	#3837	Lavender Ultra Dark
+  92,114,148, 	#3838	Lavender Blue Dark
+  123,142,171,	#3839	Lavender Blue Medium
+  176,192,218,	#3840	Lavender Blue Light
+  205,223,237,	#3841	Baby Blue Pale
+  50,102,124,	  #3842	Wedgewood Very Dark
+  20,170,208,	  #3843	Electric Blue
+  18,174,186,	  #3844	Turquoise Bright Dark
+  4,196,202,	  #3845	Turquoise Bright Medium
+  6,227,230,	  #3846	Turquoise Bright Light
+  52,125,117,	  #3847	Teal Green Dark
+  85,147,146,	  #3848	Teal Green Medium
+  82,179,164,	  #3849	Teal Green Light
+  55,132,119,	  #3850	Green Bright Dark
+  73,179,161,	  #3851	Green Bright Light
+  205,157,55,	  #3852	Straw Very Dark
+  242,151,70,	  #3853	Autumn Gold Dark
+  242,175,104,	#3854	Autumn Gold Medium
+  250,211,150,	#3855	Autumn Gold Light
+  255,211,181,	#3856	Mahogany Ultra Very Light
+  104,37,26,	  #3857	Rosewood Dark
+  150,74,63,	  #3858	Rosewood Medium
+  186,139,124,	#3859	Rosewood Light
+  125,93,87,	  #3860	Cocoa
+  166,136,129,	#3861	Cocoa Light
+  138,110,78,	  #3862	Mocha Beige Dark
+  164,131,92,	  #3863	Mocha Beige Medium
+  203,182,156,	#3864	Mocha Beige Light
+  249,247,241,	#3865	Winter White
+  250,246,240,	#3866	Mocha Brown Ultra Very Light
+]
--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 import sys      
 import math
 import numpy
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImagePalette
+from dmc_colors import DMC_RGB_COLORS
 
-AUTO_SAMPLING_SCALING = 3
+# DMC_RGB_COLORS=[
+#   199,43,59,    #321	Red
+#   90,143,184,   #322	Baby Blue Dark
+#   179,59,75,    #326	Rose Very Dark
+#   99,54,102,    #327	Violet Dark
+# ]
+
+DEFAULT_SAMPLING_SCALE = 4
 DEFAULT_RENDER_PIXEL_SIZE = 8
 DEFAULT_GAP = 1
-DEFAULT_COLOR_COUNT = 54
+DEFAULT_GAP_COLOR = (66,66,66)  #3799	Pewter Gray Very Dark
+DEFAULT_COLOR_COUNT = 256
 
 class Main():
   def __init__(self):
     if (len(sys.argv) < 2):
       return print('Error [init]: need an image file in argument')
-    # open image
-    base_image = self.open_image(sys.argv[1])
-    if (base_image == False):
-      return
     # get params
-    self.get_params(base_image)
+    base_image = self.get_params(sys.argv[1])
+    # gap & board size
+    self.gap = DEFAULT_GAP if len(sys.argv) < 3 else int(sys.argv[2])
+    self.board_size = base_image.size[0] // self.sampling_size, base_image.size[1] // self.sampling_size
     # process image
-    print(f'[1/4] resize & color reduce image..')
-    image = self.reduce_colors(base_image, self.color_count)
-    print('[2/4] generating pixels..')
+    print(f'[1/5] creating colors palette..')
+    palette_image = self.create_color_palette(self.color_count)
+    print(f'[2/5] resize & color reduce image..')
+    image = self.reduce_colors(base_image, palette_image)
+    print('[3/5] generating pixels..')
     pixels = self.process_pixels(image)
-    print('[3/4] creating image..')
+    print('[4/5] creating image..')
     new_image = self.draw_image(pixels)
-    print('[4/4] rendering..')
+    print('[5/5] rendering..')
     # show image
     new_image.show()
     
-  def open_image(self, file_path):
-    try:
-      return Image.open(file_path, "r")
-    except:
-      print(f'Error [init]: File \'{file_path}\' not found')
-      return False
-  
   def get_params(self, image):
+    image = Image.open(sys.argv[1], "r")
     width, height = image.size
     min_size = width if width < height else height
     # auto sampling
-    auto_sampling = round(math.sqrt(min_size) / AUTO_SAMPLING_SCALING)
+    auto_sampling = round(math.sqrt(min_size) / DEFAULT_SAMPLING_SCALE)
     print(f'Echantillonnage de l\'image: (defaut: {auto_sampling})')
     self.sampling_size = int(input() or auto_sampling)
     # def board size
@@ -51,19 +55,35 @@ class Main():
     self.render_pixel_size = int(input() or proposed_pixel_size)
     print(f'Nombre limite de couleurs: (defaut: {DEFAULT_COLOR_COUNT})')
     self.color_count = int(input() or DEFAULT_COLOR_COUNT)
+    return image
+
+  def create_color_palette(self, color_count):
+    palette_image = Image.new('P', (1,1))
+    rgb_color_count = color_count * 3
+    max_rgb_color_count = rgb_color_count if rgb_color_count < len(DMC_RGB_COLORS) else len(DMC_RGB_COLORS)
+    rgb_colors = DMC_RGB_COLORS[:max_rgb_color_count]
+    self.color_count = len(rgb_colors) // 3
+    print(f'\t↪ color count: {self.color_count}')
+    palette_image.putpalette(rgb_colors)
+    return palette_image
   
-  def reduce_colors(self, image, color_count):
-    processed_image = image.resize(self.board_size).convert("P", palette=Image.ADAPTIVE, colors=color_count)
-    print(f'\t↪ new image size: {self.board_size}')
-    print(f'\t↪ colors limited to {color_count}')
+  def reduce_colors(self, image, palette_image):
+    processed_image = image.resize(self.board_size)
+    px = processed_image.load()
+    for x in range(processed_image.width):
+      for y in range(processed_image.height):
+        pixel = processed_image.getpixel((x, y))
+        print(pixel)
+    #.convert("RGB").quantize(palette=palette_image)
+    # resize_image = image.resize(self.board_size).convert("RGB")
+    # processed_image = resize_image.quantize(palette=palette_image, dither=Image.Dither.NONE)
     return processed_image.convert("RGB")
     
   def process_pixels(self, image):
     pixels = list(image.getdata())
-    colors = []
+    colors = set()
     for pixel in pixels:
-      if pixel not in colors:
-        colors.append(pixel)
+      colors.add(pixel)
     print(f'\t↪ {len(pixels)} pixels generated')
     print(f'\t↪ {len(colors)} different colors')
     return pixels
@@ -71,21 +91,17 @@ class Main():
   def draw_pixel(self, image, coords, color):
     for y in range(self.render_pixel_size):
       for x in range(self.render_pixel_size):
-        try:
-          pos_x = DEFAULT_GAP * (coords[0] + 1) + coords[0] * self.render_pixel_size + x
-          pos_y = DEFAULT_GAP * (coords[1] + 1) + coords[1] * self.render_pixel_size + y
-          image.putpixel((pos_x, pos_y), color)
-        except:
-          print(f'nope {pos_x, pos_y}')
+        pos_x = self.gap * (coords[0] + 1) + coords[0] * self.render_pixel_size + x
+        pos_y = self.gap * (coords[1] + 1) + coords[1] * self.render_pixel_size + y
+        image.putpixel((pos_x, pos_y), color)
  
-    
   def draw_image(self, pixels):
-    image_size_x = self.board_size[0] * (self.render_pixel_size + DEFAULT_GAP) + DEFAULT_GAP
-    image_size_y = self.board_size[1] * (self.render_pixel_size + DEFAULT_GAP) + DEFAULT_GAP
+    image_size_x = self.board_size[0] * (self.render_pixel_size + self.gap) + self.gap
+    image_size_y = self.board_size[1] * (self.render_pixel_size + self.gap) + self.gap
     image_size = image_size_x, image_size_y
     print(f'\t↪ image size: {image_size}')
     print(f'\t↪ board size: {self.board_size}')
-    image = Image.new(mode="RGB", size=image_size)
+    image = Image.new("RGB", image_size, DEFAULT_GAP_COLOR)
     for y in range(self.board_size[1]):
       for x in range(self.board_size[0]):
         index = x + y * self.board_size[0]
